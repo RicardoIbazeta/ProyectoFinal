397b023ccac1455ba785fcf0688be700dcb32c64
Ricardo Ibazeta <picuibazeta@gmail.com>
2023-11-09 18:51:34 -0300

    9/11/2023 creacion entidades y servicio

--- src/main/java/Egg/ProyectoFinal/entidades/Contratacion.java
+++ src/main/java/Egg/ProyectoFinal/entidades/Contratacion.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Egg.ProyectoFinal;
+package Egg.ProyectoFinal.entidades;
 
 /**
  *
- * @author agust
+ * @author Picu Ibazeta
  */
-public class html {
+public class Contratacion {
     
 }
