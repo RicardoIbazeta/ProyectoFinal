828ff02fa59794dbd1bf0abae958b724b5770e24
Ricardo Ibazeta <picuibazeta@gmail.com>
2023-11-09 17:43:30 -0300

    creacion paquete entidades

---
+++ src/main/java/Egg/ProyectoFinal/entidades/Cliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Egg.ProyectoFinal.entidades;
+
+/**
+ *
+ * @author Picu Ibazeta
+ */
+public class Cliente {
+    
+}
