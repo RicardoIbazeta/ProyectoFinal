090a870109be3b48cb6d45c781be2629909c2d5a
Nahuel121312 <129802185+Nahuel121312@users.noreply.github.com>
2023-11-10 20:23:34 -0300

    Usuario servicio

--- src/main/java/Egg/ProyectoFinal/servicios/UsuarioServicio.java
+++ src/main/java/Egg/ProyectoFinal/servicios/UsuarioServicio.java
@@ -4,7 +4,10 @@ package Egg.ProyectoFinal.servicios;
 import Egg.ProyectoFinal.entidades.Usuario;
 import Egg.ProyectoFinal.Repositorio.UsuarioRepositorio;
 import jakarta.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
@@ -33,5 +36,38 @@ public class UsuarioServicio {
         
         
     }
+    
+    public List<Usuario> listarUsuarios(){
+        
+        List<Usuario> usuarios = new ArrayList();
+        usuarios = usuariorepositorio.findAll();
+        return usuarios;
+    }
+    
+    
+    public void modificarUsuario(String id,String nombre,String apellido,String email,String contraseña,String telefono,String direccion,Boolean tipoUsuario){
+        
+        Optional<Usuario> respuesta = usuariorepositorio.findById(id);
+        
+        
+        if(respuesta.isPresent()){
+            Usuario usuario = respuesta.get();
+            usuario.setApellido(apellido);
+            usuario.setContraseña(contraseña);
+            usuario.setDireccion(direccion);
+            usuario.setEmail(email);
+            usuario.setNombre(nombre);
+            usuario.setTelefono(telefono);
+            usuario.setTipoUsuario(tipoUsuario);
+            
+            usuariorepositorio.save(usuario);
+            
+        }
+        
+        
+    }
+    
+    
+    
     
 }
