b442c79cce41a4705d6d47b24f7c8819f3b769ce
Juan Cruz Mannoia <juancruz.mannoia@gmail.com>
2023-11-13 17:33:03 -0300

    intento merge ramas

--- src/main/java/Egg/ProyectoFinal/ProyectoFinalApplication.java
+++ src/main/java/Egg/ProyectoFinal/ProyectoFinalApplication.java
@@ -1,6 +1,5 @@
 package Egg.ProyectoFinal;
 
-import Egg.ProyectoFinal.entidades.Usuario;
 import Egg.ProyectoFinal.enumeraciones.Estado;
 import Egg.ProyectoFinal.excepciones.MiException;
 import Egg.ProyectoFinal.servicios.UsuarioServicio;
@@ -13,8 +12,6 @@ public class ProyectoFinalApplication {
 	public static void main(String[] args) throws MiException {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
                 
-                //UsuarioServicio us = new UsuarioServicio();
-                //us.crearUsuario("pepe", "martinez", "40123987", "pepe@gmail.com", "123456", Estado.EN_PROCESO, "1112349876", "las colinas 123", Boolean.TRUE);
                 
 	}
 
