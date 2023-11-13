a5825c4612cded3341fef33d8803f766b26b09ff
Juan Cruz Mannoia <juancruz.mannoia@gmail.com>
2023-11-13 13:59:21 -0300

    editando index

--- src/main/java/Egg/ProyectoFinal/ProyectoFinalApplication.java
+++ src/main/java/Egg/ProyectoFinal/ProyectoFinalApplication.java
@@ -1,5 +1,8 @@
 package Egg.ProyectoFinal;
 
+import Egg.ProyectoFinal.entidades.Usuario;
+import Egg.ProyectoFinal.enumeraciones.Estado;
+import Egg.ProyectoFinal.excepciones.MiException;
 import Egg.ProyectoFinal.servicios.UsuarioServicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoFinalApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MiException {
 		SpringApplication.run(ProyectoFinalApplication.class, args);
-            
+                
+                //UsuarioServicio us = new UsuarioServicio();
+                //us.crearUsuario("pepe", "martinez", "40123987", "pepe@gmail.com", "123456", Estado.EN_PROCESO, "1112349876", "las colinas 123", Boolean.TRUE);
                 
 	}
 
