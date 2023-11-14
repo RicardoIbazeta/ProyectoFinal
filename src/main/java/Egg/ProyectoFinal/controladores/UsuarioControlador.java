540992c50daf88b1b939004f7c0a07be158b0e44
Juan Cruz Mannoia <juancruz.mannoia@gmail.com>
2023-11-14 16:45:55 -0300

    rama front

--- src/main/java/Egg/ProyectoFinal/controladores/UsuarioControlador.java
+++ src/main/java/Egg/ProyectoFinal/controladores/UsuarioControlador.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/usuario")
 public class UsuarioControlador {
     
     @Autowired
