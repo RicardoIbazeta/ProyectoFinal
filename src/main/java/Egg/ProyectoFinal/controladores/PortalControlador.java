f2539cafe6c1163b416e64415869b47658f1be84
Ricardo Ibazeta <picuibazeta@gmail.com>
2023-11-17 18:50:02 -0300

    ultima version. no ingresa session

--- src/main/java/Egg/ProyectoFinal/controladores/PortalControlador.java
+++ src/main/java/Egg/ProyectoFinal/controladores/PortalControlador.java
@@ -1,6 +1,8 @@
 package Egg.ProyectoFinal.controladores;
 
+import Egg.ProyectoFinal.entidades.Usuario;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,24 @@ public class PortalControlador {
 
     }
 
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login.html";
+//    }
+    
+    @GetMapping("/inicio")
+    public String inicio() {
+        return "inicio.html";
+    } 
+    
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(required = false)String error, ModelMap modelo){
+        if(error !=null){
+            modelo.put("error","Usuario o Contraseña Invalidos");
+            
+        }
         return "login.html";
     }
+    
 
 }
