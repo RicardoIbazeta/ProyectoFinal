209283012df0cbfd1f4a32a462c934c8ac023f62
Juan Cruz Mannoia <juancruz.mannoia@gmail.com>
2023-11-23 05:41:32 -0300

    preparando merge con back

--- src/main/java/Egg/ProyectoFinal/Repositorio/ReseniaRepositorio.java
+++ src/main/java/Egg/ProyectoFinal/Repositorio/ReseniaRepositorio.java
@@ -1,11 +1,12 @@
 
 package Egg.ProyectoFinal.Repositorio;
 
+import Egg.ProyectoFinal.entidades.Resenia;
 import Egg.ProyectoFinal.enumeraciones.Estrella;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReseniaRepositorio extends JpaRepository<Estrella,String> {
+public interface ReseniaRepositorio extends JpaRepository<Resenia,String> {
     
 }
