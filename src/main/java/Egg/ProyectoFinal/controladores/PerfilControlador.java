5684aceb0168b7de4369b1a2def71fa61cbb95c0
Dante <trabalon.dante@gmail.com>
2023-11-23 23:04:24 -0300

    ver perfil no funciona

--- src/main/java/Egg/ProyectoFinal/controladores/PerfilControlador.java
+++ src/main/java/Egg/ProyectoFinal/controladores/PerfilControlador.java
@@ -1,19 +1,28 @@
 
 package Egg.ProyectoFinal.controladores;
 
+import Egg.ProyectoFinal.Repositorio.UsuarioRepositorio;
+import Egg.ProyectoFinal.entidades.Usuario;
+import Egg.ProyectoFinal.servicios.UsuarioServicio;
+import static org.hibernate.criterion.Projections.id;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/usuario/Perfil")
 public class PerfilControlador {
-
-    @GetMapping("/usuario/Perfil")
-    public String verPerfilAdmin(Model model) {
-        // Lógica para mostrar el perfil del administrador
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+    
+    //@GetMapping("/{id}/perfil")
+    public String mostrarPerfilUsuario(@PathVariable Long id, Model model) {
+        Usuario usuario = usuarioRepositorio.findById(id);
+        model.addAttribute("usuario", usuario);
         return "perfil.html";
     }
 
