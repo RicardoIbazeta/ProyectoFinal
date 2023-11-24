067a1028e1e81b6f8ec6645857dbadf1cf09511c
Nahuel121312 <129802185+Nahuel121312@users.noreply.github.com>
2023-11-24 19:57:45 -0300

    Imagen metodo eliminar,Rubro metodo modificar y eliminar

--- src/main/java/Egg/ProyectoFinal/Repositorio/ImagenRepositorio.java
+++ src/main/java/Egg/ProyectoFinal/Repositorio/ImagenRepositorio.java
@@ -3,9 +3,15 @@ package Egg.ProyectoFinal.Repositorio;
 
 import Egg.ProyectoFinal.entidades.Imagen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImagenRepositorio extends JpaRepository<Imagen,String> {
+    
+  @Query("SELECT i FROM Imagen i WHERE i.id = :id")
+  public Imagen buscarPorId(@Param("id") String id);
+    
     
 }
